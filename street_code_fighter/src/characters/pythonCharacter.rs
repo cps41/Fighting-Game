a448250b06ba86a62a493c7133ec8344eb5a1845
Kenny Meier <kem243@pitt.edu>
2021-06-13 16:44:10 -0400

    Update pythonCharacter.rs
    Edits the values for the python character

--- street_code_fighter/src/characters/pythonCharacter.rs
+++ street_code_fighter/src/characters/pythonCharacter.rs
@@ -1,27 +1,28 @@
 fn create_python_fighter() {
-    let weight = 50;
-    let gravity = 50;
-    let max_fall_speed = 50;
-    let max_ground_speed = 50;
-    let run_speed = 50;
-    let max_air_speed = 50;
-    let aerial_transition_speed = 50;
-    let crawl_speed = 50;
-    let dodge_speed = 50;
-    let friction = 50;
-    let static_grip = 50;
-    let pivot_grip = 50;
-    let air_resistance = 50;
-    let air_control = 50;
-    let jumps = 50;
-    let jump_height = 50;
-    let short_hop_height = 50;
-    let air_jump_height = 50;
-    let heavy_land_lag = 50;
-    let wavedash_lag = 50;
-    let fastfall_multiplier = 50;
-    let hitstun_elasticity = 50;
-    let shield_size = 50;
+    // These are just random values for now
+    let weight = 180;
+    let gravity = -9.8;
+    let max_fall_speed = 20;
+    let max_ground_speed = 10;
+    let run_speed = 15;
+    let max_air_speed = 5;
+    let aerial_transition_speed = 3;
+    let crawl_speed = 3;
+    let dodge_speed = 5;
+    let friction = -0.1;
+    let static_grip = 20;
+    let pivot_grip = 25;
+    let air_resistance = -0.1;
+    let air_control = 5;
+    let jumps = 2;
+    let jump_height = 10;
+    let short_hop_height = 5;
+    let air_jump_height = 7;
+    let heavy_land_lag = 2;
+    let wavedash_lag = 2;
+    let fastfall_multiplier = 1.25;
+    let hitstun_elasticity = 2.5;
+    let shield_size = 3;
 
     let mut python_fighter = Fighter{ weight, gravity, max_fall_speed, max_ground_speed,
                                       run_speed, max_air_speed, aerial_transition_speed, crawl_speed, 
