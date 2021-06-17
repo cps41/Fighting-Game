d2d475bd09b1439dd8f7f85f7e4c4f3c99072900
cps41 <cps41@pitt.edu>
2021-06-17 18:55:23 -0400

    remove folder

--- street_code_fighter/target/debug/deps/bitflags-36bf4a3fde12e597.d
+++
@@ -1,7 +0,0 @@
-C:\Users\asanders\documents\Fighting-Game\street_code_fighter\target\debug\deps\bitflags-36bf4a3fde12e597.rmeta: C:\Users\asanders\.cargo\registry\src\github.com-1ecc6299db9ec823\bitflags-1.2.1\src\lib.rs
-
-C:\Users\asanders\documents\Fighting-Game\street_code_fighter\target\debug\deps\libbitflags-36bf4a3fde12e597.rlib: C:\Users\asanders\.cargo\registry\src\github.com-1ecc6299db9ec823\bitflags-1.2.1\src\lib.rs
-
-C:\Users\asanders\documents\Fighting-Game\street_code_fighter\target\debug\deps\bitflags-36bf4a3fde12e597.d: C:\Users\asanders\.cargo\registry\src\github.com-1ecc6299db9ec823\bitflags-1.2.1\src\lib.rs
-
-C:\Users\asanders\.cargo\registry\src\github.com-1ecc6299db9ec823\bitflags-1.2.1\src\lib.rs:
--- street_code_fighter/target/debug/deps/street_code_fighter.d
+++
@@ -1,5 +0,0 @@
-C:\Users\asanders\Documents\fighting-game\street_code_fighter\target\debug\deps\street_code_fighter.exe: src\main.rs
-
-C:\Users\asanders\Documents\fighting-game\street_code_fighter\target\debug\deps\street_code_fighter.d: src\main.rs
-
-src\main.rs:
