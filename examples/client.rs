21b081e648cda3aeeb288d492c075b123b091395
Alexis Sanders <asanders@merkleinc.com>
2021-06-25 22:12:51 -0400

    adding examples of client/server

--- examples/client.rs
+++ examples/client.rs
@@ -4,6 +4,7 @@ use std::{io, str};
 
 pub fn main() -> std::io::Result<()> {
 
+	// ADDRESSING
 	let client_addresses: [SocketAddr; 1] = [
 	    SocketAddr::from(([127, 0, 0, 1], 1667)),
 	    // can add backup client IPs
@@ -14,15 +15,20 @@ pub fn main() -> std::io::Result<()> {
         // can add backup server IPs
     ];
 
+    // BINDING & CONNECTING
     let mut socket = UdpSocket::bind(&client_addresses[..]).expect("couldn't bind to address");
-
     socket.connect(&server_addresses[..]).expect("connect function failed");
 
-	socket.send(&[1,6,6,6]).expect("couldn't send message"); // Test message
 
-  	// loop { // sending chat side
+    // SENDING
+	// socket.send(&[1,6,6,6]).expect("couldn't send message"); // Test message
+    'sending: loop { // sending client side
+    	println!("input: ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        socket.send(input.as_bytes())?;	
+        // note: use ctrl+c to exit
+      } // close sending loop
 
- 	// }
-
- 	Ok(())
-}
+    Ok(())
+} // close main fn
--- examples/server.rs
+++ examples/server.rs
@@ -1,26 +1,36 @@
 use std::net::{SocketAddr, UdpSocket};
-use std::time::Instant;
+use std::time::{SystemTime,UNIX_EPOCH};
 use std::str;
 
-pub fn main() {
+pub fn main() -> Result<(), String>{
 
+    // ADDRESSING
     let server_addresses: [SocketAddr; 1] = [
         SocketAddr::from(([127, 0, 0, 1], 1666)),
         // can add backup IPs
     ];
 
-    
+    // BINDING
     let socket = UdpSocket::bind(&server_addresses[..]).expect("couldn't bind to address");
 
-    // loop { // listening loop
+    // LISTENING
+    'listening: loop { // listening loop
 
+        let mut buffer = [0u8; 4096]; // a buffer than accepts 4096 
 
-    //  } // close listening loop
+        match socket.recv(&mut buffer) {
+            Ok(received) => { 
+                // println!("received {} bytes {:?}", received, &buffer[..received]); // test to print bytes and buffer
+                let system_time = SystemTime::now(); // get system time
+                let ugly_text = str::from_utf8(&buffer).unwrap(); // get the text from buffer
+                let pretty_text = ugly_text.trim_matches(char::from(0)); // cut the trailing 0s
+                println!("{:?}: {:?}", system_time, pretty_text); // print to console
+            }, 
+            Err(e) => println!("recv function failed: {:?}", e),
+        } // deal with Result that's recieved from the buffer
 
-    let mut buf = [0; 10];
-    match socket.recv(&mut buf) {
-        Ok(received) => println!("received {} bytes {:?}", received, &buf[..received]),
-        Err(e) => println!("recv function failed: {:?}", e),
-    }
+        // note: use ctrl+c to exit
+    } // close listening loop
 
+    Ok(())
 } // close main fn
