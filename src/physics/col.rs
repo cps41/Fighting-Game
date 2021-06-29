73cb22bcd8664ae0e1f358f0a34c9d8c863a5709
cps41 <cps41@pitt.edu>
2021-06-29 08:22:46 -0400

    tryin

---
+++ src/physics/col.rs
@@ -0,0 +1,9 @@
+#![allow(non_snake_case)]
+use sdl2::rect::Rect;
+
+type Link<T> = Option<Box<T>>
+
+struct Node {
+    elem: BVHNode,
+    
+}
--- src/physics/collisions.rs
+++ src/physics/collisions.rs
@@ -1,11 +1,22 @@
 #![allow(non_snake_case)]
 use sdl2::rect::Rect;
+use std::rc::Rc;
+use std::collections::HashMap;
+use std::cell::{Ref, RefMut, RefCell};
 
 pub fn check_collision(a: &CollisionObject, b: &CollisionObject) -> bool {
 	if let CollisionObjectType::HurtBox = a.obj_type {
 		if let CollisionObjectType::HurtBox = b.obj_type {return false}
 	}
 	reg_collision(&a.rect, &b.rect)
+}
+
+fn makeLink<T>(node: T) -> Link<T> {
+	Some(Box::new(node))
+}
+
+fn makeParLink (node: *const BVHNode) -> ParentLink<*const BVHNode> {
+	Some(Rc::new(RefCell::new(node)))
 }
 
 fn reg_collision(a: &Rect, b: &Rect) -> bool {
@@ -46,7 +57,6 @@ pub enum CollisionObjectType {
     Hazard,
     Platform,
     Wall,
-	Empty,
 }
 
 type PotentialCollision = (CollisionObject, CollisionObject);
@@ -83,50 +93,62 @@ impl CollisionObject {
 	fn overlapsWith(&self, other: &CollisionObject) -> bool {
 		check_collision(self, other)
 	}
-
-	fn isEmpty(&self) -> bool {
-		match self.obj_type {
-			CollisionObjectType::Empty => true,
-			_ => false,
-		}
-	}
-}
-
-trait Empty<T> {
-	fn empty() -> T;
-}
-
-impl Empty<CollisionObject> for CollisionObject {
-	fn empty() -> CollisionObject {
-		CollisionObject{obj_type: CollisionObjectType::Empty, area: 0, rect: Rect::new(0, 0, 0, 0)}
-	}
 }
 
 // Link type for child nodes
 type Link<T> = Option<Box<T>>;
 
-trait Refer<T> {
-	fn refer<'a>(&'a self) -> &'a T;
+type ParentLink<T> = Option<Rc<RefCell<T>>>;
+
+trait Refer<'a, T> {
+	fn refer(&'a self) -> &'a T;
+	fn defer(&'a self) -> &'a T;
 }
 
-impl<T> Refer<T> for Link<T> {
-	fn refer<'a>(&'a self) -> &'a T {
+impl<'a, T> Refer<'a, T> for Link<T> {
+	fn refer(&'a self) -> &'a T {
 		self.as_ref().unwrap()
+	}
+
+	fn defer(&'a self) -> &'a T {
+		self.as_deref().unwrap()
+	}
+}
+
+trait Defer<'a, T> {
+	fn defer(&self) -> &RefCell<T>;
+}
+
+impl<'a> Defer<'a, BVHNode> for ParentLink<BVHNode> {	
+	fn defer(&self) -> &RefCell<BVHNode> {
+		self.as_deref().unwrap()
 	}
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct BVHNode {
+	parent: ParentLink<*const BVHNode>,
 	children: (Link<BVHNode>, Link<BVHNode>),
 	obj: Link<CollisionObject>,
 	area: Rect,
 }
 
 impl BVHNode {
-	fn new(children: (Link<BVHNode>, Link<BVHNode>), obj: Link<CollisionObject>, area: Rect) -> BVHNode {
-		let mut node = BVHNode{children: children, obj: obj, area: area};
+	fn new(children: (Link<BVHNode>, Link<BVHNode>), obj: Link<CollisionObject>) -> BVHNode {
+		let mut node = BVHNode{parent: None, children: children, obj: obj, area: Rect::new(0,0,0,0)};
 		node.calculateArea();
 		node
+	}
+
+	fn newp(parent: ParentLink<*const BVHNode>, children: (Link<BVHNode>, Link<BVHNode>), obj: Link<CollisionObject>) -> BVHNode {
+		let mut node = BVHNode{parent: parent, children: children, obj: obj, area: Rect::new(0,0,0,0)};
+		node.calculateArea();
+		node
+	}
+
+	fn setParent(&mut self, parent: ParentLink<*const BVHNode>) {
+		self.parent = parent;
+		self.calculateArea();
 	}
 
 	fn calculateArea(&mut self) {
@@ -151,12 +173,20 @@ impl BVHNode {
 		else {
 			self.area = self.children.1.refer().area.union(self.children.0.refer().area); // area = smallest bounding box around both children if node has two children
 		}
+
+		if self.hasParent() {self.parent.defer().borrow_mut().calculateArea();}
+		println!("New Area: {}", self.area.area());
 	}
 
 	fn isLeaf(&self) -> bool {
 		if let None = self.obj {
 			false // node is a leaf iff node points to collision object
 		}
+		else {true}
+	}
+
+	fn hasParent(&self) -> bool {
+		if let None = self.parent {false}
 		else {true}
 	}
 
@@ -200,18 +230,10 @@ impl BVHNode {
 	}
 
 	fn insert(&mut self, new_obj: CollisionObject) {
-		if self.isLeaf() {
-			self.children.0 = Some(Box::new(BVHNode::new((None, None), self.obj.take(), self.area)));
-			self.children.1 = Some(Box::new(BVHNode::new((None, None), Some(Box::new(new_obj)), Rect::new(0, 0, 0, 0))));
-		}
+		if !self.isLeaf() {
+			let size0 = self.children.0.refer().area.area();
+			let size1 = self.children.1.refer().area.area();
 
-		else {
-			let size0 = self.children.0.as_deref().map(|node| {
-				node.area.area()
-			});
-			let size1 = self.children.1.as_deref().map(|node| {
-				node.area.area()
-			});
 			if size0 <= size1 {
 				self.children.0.as_mut().unwrap().insert(new_obj);
 			}
@@ -219,23 +241,51 @@ impl BVHNode {
 				self.children.1.as_mut().unwrap().insert(new_obj);
 			}
 		}
+		else {
+			let addr = &*self as *const _;
+			self.children.0 = makeLink(BVHNode::newp(makeParLink(addr), (None, None), self.obj.take()));
+			self.children.1 = makeLink(BVHNode::newp(makeParLink(self.clone()), (None, None), makeLink(new_obj)));
+		}
 		self.calculateArea();
 	}
 
-	fn remove(&mut self, parent: &mut Link<BVHNode>) {
-		if let Some(parent_node) = parent {
-			let mut sibling: Link<BVHNode>;
-			if parent_node.children.0 == Some(Box::new(self.clone())) {sibling = parent_node.children.1.take();}
-			else {sibling = parent_node.children.0.take();}
+	fn removeChild(&mut self, child: bool) {
+		if self.isLeaf() {return}
 
-			*parent = sibling.take();
-			parent.as_deref_mut().map(|par| {
-				par.calculateArea();
-			});
+		if child {
+			self.obj = self.children.1.as_deref_mut().unwrap().obj.take();
 		}
+		else {
+			self.obj = self.children.0.as_deref_mut().unwrap().obj.take();
+		}
+		self.children.0.take();
+		self.children.1.take();
+		self.calculateArea();
+	}
 
-		self.children.0.as_deref_mut().take();
-		self.children.1.as_deref_mut().take();
+	fn remove(&self, parent: Option<&mut BVHNode>) -> &Self {
+		if let Some(parent) = parent {
+			match parent.children.0.as_deref().take() {
+				Some(child0) => {
+					if child0 == &self.clone() {
+						println!("child 0 removed");
+						parent.obj = parent.children.1.defer().obj.clone();
+					}
+					else {
+						println!("child 1 removed");
+						parent.obj = child0.obj.clone();
+					}
+				}
+				None => {
+					println!("child 0 removed");
+					parent.obj = parent.children.1.defer().obj.clone();
+				}
+			}
+			parent.children.0.take();
+			parent.children.1.take();
+			parent.calculateArea();
+		}
+		self
 	}
 }
 
@@ -253,7 +303,7 @@ mod test {
 
 	#[test]
 	fn testBVHNodeInit() {
-		let mut node = BVHNode::new((None, None), None, Rect::new(0, 0, 0, 0));
+		let mut node = BVHNode::new((None, None), None);
 
 		assert_eq!(node.children.0, None);
 		assert_eq!(node.children.1, None);
@@ -263,37 +313,69 @@ mod test {
 
 	#[test]
 	fn testBVHNodeInsert() {
-		let mut node = BVHNode::new((None, None), 
-									Some(Box::new(
-										CollisionObject::new(CollisionObjectType::HitBox, 
-										7, 5, 4, 7))), 
-										Rect::new(0, 0, 0, 0));
+		let node7547 = BVHNode::new((None, None), makeLink(CollisionObject::new(CollisionObjectType::HitBox, 7, 5, 4, 7)));
+		let node55410 = BVHNode::new((None, None), makeLink(CollisionObject::new(CollisionObjectType::HitBox, 5, 5, 4, 10)));
+		let node58212 = BVHNode::new((None, None), makeLink(CollisionObject::new(CollisionObjectType::HitBox, 5, 8, 2, 12)));
+
+		let node = &mut node7547.clone();
 		node.insert(CollisionObject::new(CollisionObjectType::HitBox, 5, 5, 4, 10));
 
-		assert_ne!(node.children.0, None);
-		assert_ne!(node.children.1, None);
+		assert_eq!(node.parent, None);
 		assert_eq!(node.obj, None);
 		assert_eq!(node.area, Rect::new(5,5,6,10));
-		assert_eq!(*node.children.0.refer(), BVHNode{children: (None, None),
-													obj: Some(Box::new(CollisionObject::new(CollisionObjectType::HitBox, 7,5,4,7))), 
-													area: Rect::new(7,5,4,7)});
-		assert_eq!(*node.children.1.refer(), BVHNode{children: (None, None),
-													obj: Some(Box::new(CollisionObject::new(CollisionObjectType::HitBox, 5,5,4,10))), 
-													area: Rect::new(5,5,4,10)});
-													
+		
+		let mut parent = makeParLink(BVHNode::new((makeLink(node7547.clone()), makeLink(node55410.clone())), None));
+		assert_eq!(node.children.0.refer().parent, parent);
+		assert_eq!(node.children.0.refer().children, (None, None));
+		assert_eq!(node.children.0.refer().area.area(), 28);
+		assert_eq!(node.children.0.refer().obj, node7547.obj);
+		assert_eq!(node.children.1.refer().parent, parent);
+		assert_eq!(node.children.1.refer().children, (None, None));
+		assert_eq!(node.children.1.refer().area.area(), 40);
+		assert_eq!(node.children.1.refer().obj, node55410.obj);
+
 		node.insert(CollisionObject::new(CollisionObjectType::Hazard, 5, 8, 2, 12));
 		let cur = node.children.0.as_deref_mut().unwrap();
 
-		assert_ne!(cur.children.0, None);
-		assert_ne!(cur.children.1, None);
-		assert_eq!(cur.obj, None);
-		assert_eq!(cur.area, Rect::new(5,5,6,15));
 		assert_eq!(node.area, Rect::new(5,5,6,15));
-		assert_eq!(*cur.children.0.refer(), BVHNode{children: (None, None),
-													obj: Some(Box::new(CollisionObject::new(CollisionObjectType::HitBox, 7,5,4,7))), 
-													area: Rect::new(7,5,4,7)});
-		assert_eq!(*cur.children.1.refer(), BVHNode{children: (None, None),
-													obj: Some(Box::new(CollisionObject::new(CollisionObjectType::Hazard, 5,8,2,12))), 
-													area: Rect::new(5,8,2,12)});
+
+		// parent = makeParLink(BVHNode::new((makeLink(node7547.clone()), makeLink(node55410.clone())), None));
+		// assert_eq!(node.children.0.refer().parent, parent);
+		// assert_eq!(node.children.0.refer().children, (None, None));
+		// assert_eq!(node.children.0.refer().area.area(), 28);
+		// assert_eq!(node.children.0.refer().obj, node7547.obj);
+		// assert_eq!(node.children.1.refer().parent, parent);
+		// assert_eq!(node.children.1.refer().children, (None, None));
+		// assert_eq!(node.children.1.refer().area.area(), 40);
+		// assert_eq!(node.children.1.refer().obj, node55410.obj);
+	}
+
+	#[test]
+	fn testBVHNodeRemove() {
+		let node7547 = BVHNode::new((None, None), makeLink(CollisionObject::new(CollisionObjectType::HitBox, 7, 5, 4, 7)));
+		let node55410 = BVHNode::new((None, None), makeLink(CollisionObject::new(CollisionObjectType::HitBox, 5, 5, 4, 10)));
+		let node58212 = BVHNode::new((None, None), makeLink(CollisionObject::new(CollisionObjectType::HitBox, 5, 8, 2, 12)));
+
+		let node = &mut node7547.clone();
+		node.insert(CollisionObject::new(CollisionObjectType::HitBox, 5, 5, 4, 10));
+		node.insert(CollisionObject::new(CollisionObjectType::Hazard, 5, 8, 2, 12));
+		let child0 = node.children.0.as_deref_mut().unwrap();
+		let removed = child0.children.1.as_deref_mut().unwrap().remove();
+
+		assert_eq!(removed.obj.as_deref().unwrap(), &CollisionObject::new(CollisionObjectType::Hazard, 5, 8, 2, 12));
+		assert_eq!(node.parent, None);
+		assert_eq!(node.obj, None);
+		// assert_eq!(node.area, Rect::new(5,5,6,10));
+		
+		let mut parent = makeParLink(BVHNode::new((makeLink(node7547.clone()), makeLink(node55410.clone())), None));
+		assert_eq!(node.children.0.refer().parent, parent);
+		assert_eq!(node.children.0.refer().children, (None, None));
+		assert_eq!(node.children.0.refer().area.area(), 28);
+		assert_eq!(node.children.0.refer().obj, node7547.obj);
+		assert_eq!(node.children.1.refer().parent, parent);
+		assert_eq!(node.children.1.refer().children, (None, None));
+		assert_eq!(node.children.1.refer().area.area(), 40);
+		assert_eq!(node.children.1.refer().obj, node55410.obj);
+		let cur = node.children.0.as_deref_mut().unwrap();
 	}
 }
