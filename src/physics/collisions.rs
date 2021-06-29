adb10de651b5453f62e167639ffce489c116e9e8
cps41 <cps41@pitt.edu>
2021-06-29 14:48:41 -0400

    broken up

--- src/physics/collisions.rs
+++ src/physics/collisions.rs
@@ -1,60 +1,25 @@
 #![allow(non_snake_case)]
 use sdl2::rect::Rect;
-use std::cell::{self, RefCell, Ref, RefMut};
+use std::cell::{self, RefCell};
 use std::fmt;
 use std::ops::{Deref, DerefMut};
 use std::rc::{Rc, Weak};
 
+use crate::physics::nodes::{NodeRef, Link, WeakLink, BoxRef};
 
-
-pub struct NodeRef<T>(Rc<RefCell<Node<T>>>);
-
-type Link<T> = Option<Rc<RefCell<Node<T>>>>;
-type WeakLink<T> = Option<Weak<RefCell<Node<T>>>>;
+fn boxUp<T>(data: T) -> BoxRef<T>{
+	Some(Box::new(data))
+}
 
 #[derive(Debug)]
-struct Node<T> {
-    parent: WeakLink<T>,
-    left: Link<T>,
-    right: Link<T>,
-    node: T,
+pub struct Node<T> {
+    pub parent: WeakLink<T>,
+    pub left: Link<T>,
+    pub right: Link<T>,
+    pub bv: BoxRef<T>, // bounding volume
+	pub area: Rect, // total bounding area of children
 }
 
-impl<T> NodeRef<T> {
-	pub fn new(node: T) -> Self {
-		NodeRef(Rc::new(RefCell::new(
-			Node{
-				parent: None,
-				left: None,
-				right: None,
-				node: node
-			}
-		)))
-	}
-
-	pub fn getParent(&self) -> Option<NodeRef<T>> {
-        Some(NodeRef(self.0.borrow().parent.as_ref().unwrap().upgrade().unwrap()))
-	}
-}
-
-
-fn is<T>(a: &Rc<T>, b: &Rc<T>) -> bool {
-    let a = &**a as *const T;
-    let b = &**b as *const T;
-    a == b
-}
-
-impl<T> Clone for NodeRef<T> {
-    fn clone(&self) -> NodeRef<T> {
-        NodeRef(self.0.clone())
-    }
-}
-
-impl<T: fmt::Debug> fmt::Debug for NodeRef<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        fmt::Debug::fmt(&*self.0.borrow(), f)
-    }
-}
 
 pub fn check_collision(a: &CollisionObject, b: &CollisionObject) -> bool {
 	if let CollisionObjectType::HurtBox = a.obj_type {
@@ -71,9 +36,9 @@ fn reg_collision(a: &Rect, b: &Rect) -> bool {
 		{
 			false
 		}
-		else {
-			true
-		}
+	else {
+		true
+	}
 }
 
 pub fn resist(vel: i32, deltav: i32) -> i32 {
@@ -104,9 +69,9 @@ pub enum CollisionObjectType {
 	Empty,
 }
 
-type PotentialCollision = (CollisionObject, CollisionObject);
+pub type PotentialCollision = (CollisionObject, CollisionObject);
 
-trait Area {
+pub trait Area {
 	fn area(&self) -> u32;
 }
 
@@ -118,9 +83,9 @@ impl Area for Rect {
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct CollisionObject {
-    obj_type: CollisionObjectType,
-	area: u32,
-    rect: Rect,
+    pub obj_type: CollisionObjectType,
+	pub area: u32,
+    pub rect: Rect,
 }
 
 impl CollisionObject {
@@ -138,158 +103,32 @@ impl CollisionObject {
 	fn overlapsWith(&self, other: &CollisionObject) -> bool {
 		check_collision(self, other)
 	}
-
-	fn isEmpty(&self) -> bool {
-		match self.obj_type {
-			CollisionObjectType::Empty => true,
-			_ => false,
-		}
-	}
 }
 
-trait Empty<T> {
-	fn empty() -> T;
+trait Unbox<T> {
+	fn unbox<'a> (&'a self) -> &'a mut T;
 }
 
-impl Empty<CollisionObject> for CollisionObject {
-	fn empty() -> CollisionObject {
-		CollisionObject{obj_type: CollisionObjectType::Empty, area: 0, rect: Rect::new(0, 0, 0, 0)}
-	}
-}
-
-trait Refer<T> {
-	fn refer<'a>(&'a self) -> &'a T;
-}
-
-impl<T> Refer<T> for Link<T> {
-	fn refer<'a>(&'a self) -> &'a T {
-		
-	}
-}
-
-#[derive(Clone, Debug)]
-pub struct BVHNode {
-	obj: Link<CollisionObject>,
-	area: Rect,
-}
-
-impl BVHNode {
-	fn new(children: (Link<BVHNode>, Link<BVHNode>), obj: Link<CollisionObject>, area: Rect) -> BVHNode {
-		let mut node = BVHNode{obj: obj, area: area};
-		node.calculateArea();
-		node
-	}
-
-	fn calculateArea(&mut self) {
-		if let None = self.children.0 {
-			if let None = self.children.1 {
-				if let None = self.obj {
-					// !!!!! shouldn't be possible in gameplay, here for testing !!!!!
-					self.area = Rect::new(0, 0, 0, 0); // area of 0 if node has no children and points to no collision object
-				}
-				else {
-					self.area = self.obj.refer().rect.clone(); // area = area of collision object if node has no children but points to object
-				}
-			}
-			else {
-				self.area = self.children.1.refer().area.clone(); // area = children.1 if node has (None, Some)
-			}
-		}
-
-		else if let None = self.children.1 {
-			self.area = self.children.0.refer().area.clone(); // area = children.0 if node has (Some, None)
-		}
-		else {
-			self.area = self.children.1.refer().area.union(self.children.0.refer().area); // area = smallest bounding box around both children if node has two children
+impl Node<CollisionObject> {
+	pub fn new(parent: WeakLink<CollisionObject>, bv: CollisionObject) -> Self {
+		Node{
+			parent: parent,
+			left: None,
+			right: None,
+			bv: boxUp(bv),
+			area: Rect::new(0,0,0,0),
 		}
 	}
 
-	fn isLeaf(&self) -> bool {
-		if let None = self.obj {
+	pub fn isLeaf(&self) -> bool {
+		if let None = self.bv {
 			false // node is a leaf iff node points to collision object
 		}
 		else {true}
 	}
-
-	fn overlapsWith(&self, other: &BVHNode) -> bool {
-		self.area.has_intersection(other.area)
-	}
-
-	fn collidingWith(& self, other: & BVHNode, potential: &mut Option<PotentialCollision>, limit: i32) -> i32 {
-		if !self.overlapsWith(other) || limit == 0 {return 0;}
-
-		if self.isLeaf() && other.isLeaf() {
-			potential.as_mut().unwrap().0 = self.obj.refer().clone();
-			potential.as_mut().unwrap().1 = self.obj.refer().clone();
-			return 1;
-		}
-
-		if other.isLeaf() || (!self.isLeaf() && self.area.area() >= other.area.area()) {
-			let count = self.children.0.refer().collidingWith(self.children.1.refer(), potential, limit);
-
-			if limit > count {
-				return count + self.children.1.refer().collidingWith(other, potential, limit);
-			}
-
-			else {return count;}
-		}
-
-		else {
-			let count = self.collidingWith(self.children.0.refer(), potential, limit);
-
-			if limit > count {
-				return count + self.collidingWith(self.children.1.refer(), potential, limit);
-			}
-
-			else {return count;}
-		}
-	}
-
-	fn getPotentialCollsions(&self, potential: &mut Option<PotentialCollision>, limit: i32) -> i32{
-		if self.isLeaf() || limit == 0 {return 0;}
-		self.children.0.refer().collidingWith(self.children.1.refer(), potential, limit)
-	}
-
-	fn insert(&mut self, new_obj: CollisionObject) {
-		if self.isLeaf() {
-			self.children.0 = Some(Box::new(BVHNode::new((None, None), self.obj.take(), self.area)));
-			self.children.1 = Some(Box::new(BVHNode::new((None, None), Some(Box::new(new_obj)), Rect::new(0, 0, 0, 0))));
-		}
-
-		else {
-			let size0 = self.children.0.as_deref().map(|node| {
-				node.area.area()
-			});
-			let size1 = self.children.1.as_deref().map(|node| {
-				node.area.area()
-			});
-			if size0 <= size1 {
-				self.children.0.as_mut().unwrap().insert(new_obj);
-			}
-			else {
-				self.children.1.as_mut().unwrap().insert(new_obj);
-			}
-		}
-		self.calculateArea();
-	}
-
-	fn remove(&mut self, parent: &mut Link<BVHNode>) {
-		if let Some(parent_node) = parent {
-			let mut sibling: Link<BVHNode>;
-			if parent_node.children.0 == Some(Box::new(self.clone())) {sibling = parent_node.children.1.take();}
-			else {sibling = parent_node.children.0.take();}
-
-			*parent = sibling.take();
-			parent.as_deref_mut().map(|par| {
-				par.calculateArea();
-			});
-		}
-
-		self.children.0.as_deref_mut().take();
-		self.children.1.as_deref_mut().take();
-	}
 }
 
+/*
 #[cfg(test)]
 mod test {
 	use super::*;
@@ -304,14 +143,15 @@ mod test {
 
 	#[test]
 	fn testBVHNodeInit() {
-		let mut node = BVHNode::new((None, None), None, Rect::new(0, 0, 0, 0));
+		let co = CollisionObject::new(CollisionObjectType::HitBox, 0, 2, 3, 3);
+		let node = NodeRef::new(co.clone());
 
-		assert_eq!(node.children.0, None);
-		assert_eq!(node.children.1, None);
-		assert_eq!(node.obj, None);
-		assert_eq!(node.area, Rect::new(0,0,0,0));
+		assert_eq!(node.get().left, None);
+		assert_eq!(node.get().right, None);
+		assert_eq!(node.get().bv, co);
+		assert_eq!(node.get().area, Rect::new(0,2,3,3));
 	}
-
+/*
 	#[test]
 	fn testBVHNodeInsert() {
 		let mut node = BVHNode::new((None, None), 
@@ -321,30 +161,31 @@ mod test {
 										Rect::new(0, 0, 0, 0));
 		node.insert(CollisionObject::new(CollisionObjectType::HitBox, 5, 5, 4, 10));
 
-		assert_ne!(node.children.0, None);
-		assert_ne!(node.children.1, None);
-		assert_eq!(node.obj, None);
+		assert_ne!(node.left, None);
+		assert_ne!(node.right, None);
+		assert_eq!(node.bv, None);
 		assert_eq!(node.area, Rect::new(5,5,6,10));
-		assert_eq!(*node.children.0.refer(), BVHNode{children: (None, None),
+		assert_eq!(*node.left.refer(), BVHNode{children: (None, None),
 													obj: Some(Box::new(CollisionObject::new(CollisionObjectType::HitBox, 7,5,4,7))), 
 													area: Rect::new(7,5,4,7)});
-		assert_eq!(*node.children.1.refer(), BVHNode{children: (None, None),
+		assert_eq!(*node.right.refer(), BVHNode{children: (None, None),
 													obj: Some(Box::new(CollisionObject::new(CollisionObjectType::HitBox, 5,5,4,10))), 
 													area: Rect::new(5,5,4,10)});
 													
 		node.insert(CollisionObject::new(CollisionObjectType::Hazard, 5, 8, 2, 12));
-		let cur = node.children.0.as_deref_mut().unwrap();
+		let cur = node.left.as_deref_mut().unwrap();
 
-		assert_ne!(cur.children.0, None);
-		assert_ne!(cur.children.1, None);
-		assert_eq!(cur.obj, None);
+		assert_ne!(cur.left, None);
+		assert_ne!(cur.right, None);
+		assert_eq!(cur.bv, None);
 		assert_eq!(cur.area, Rect::new(5,5,6,15));
 		assert_eq!(node.area, Rect::new(5,5,6,15));
-		assert_eq!(*cur.children.0.refer(), BVHNode{children: (None, None),
+		assert_eq!(*cur.left.refer(), BVHNode{children: (None, None),
 													obj: Some(Box::new(CollisionObject::new(CollisionObjectType::HitBox, 7,5,4,7))), 
 													area: Rect::new(7,5,4,7)});
-		assert_eq!(*cur.children.1.refer(), BVHNode{children: (None, None),
+		assert_eq!(*cur.right.refer(), BVHNode{children: (None, None),
 													obj: Some(Box::new(CollisionObject::new(CollisionObjectType::Hazard, 5,8,2,12))), 
 													area: Rect::new(5,8,2,12)});
-	}
+	}*/
 }
+*/
---
+++ src/physics/nodes.rs
@@ -0,0 +1,219 @@
+#![allow(non_snake_case)]
+use sdl2::rect::Rect;
+use std::cell::{self, RefCell};
+use std::fmt;
+use std::ops::{Deref, DerefMut};
+use std::rc::{Rc, Weak};
+use crate::physics::collisions::{Node, CollisionObject, Area, PotentialCollision};
+
+pub struct NodeRef<T>(Rc<RefCell<Node<T>>>);
+
+pub type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+pub type WeakLink<T> = Option<Weak<RefCell<Node<T>>>>;
+pub type BoxRef<T> = Option<Box<T>>;
+
+fn boxUp<T>(data: T) -> BoxRef<T>{
+	Some(Box::new(data))
+}
+
+impl<T> PartialEq for NodeRef<T> {
+	fn eq(&self, other: &NodeRef<T>) -> bool {
+		is(&self.0, &other.0)
+	}
+}
+
+
+fn is<T>(a: &Rc<T>, b: &Rc<T>) -> bool {
+    let a = &**a as *const T;
+    let b = &**b as *const T;
+    a == b
+}
+
+impl<T> Clone for NodeRef<T> {
+    fn clone(&self) -> NodeRef<T> {
+        NodeRef(self.0.clone())
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for NodeRef<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(&*self.0.borrow(), f)
+    }
+}
+
+
+trait Refer<T> {
+	fn get(&self) -> std::cell::Ref<Node<T>>;
+    fn getMut(&self) -> std::cell::RefMut<Node<T>>;
+}
+
+impl<T> Refer<T> for NodeRef<T> {
+	fn get(&self) -> std::cell::Ref<Node<T>> {
+		self.0.as_ref().borrow()
+	}
+	fn getMut(&self) -> std::cell::RefMut<Node<T>> {
+		self.0.as_ref().borrow_mut()
+	}
+}
+
+/// Wraps a `std::cell::Ref` for a node’s data.
+pub struct Ref<'a, T: 'a> {
+    _ref: cell::Ref<'a, Node<T>>
+}
+
+/// Wraps a `std::cell::RefMut` for a node’s data.
+pub struct RefMut<'a, T: 'a> {
+    _ref: cell::RefMut<'a, Node<T>>
+}
+
+impl<'a, T> Deref for Ref<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &T { &self._ref.bv.as_deref().unwrap() }
+}
+
+impl<'a, T> Deref for RefMut<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &T { &self._ref.bv.as_deref().unwrap() }
+}
+
+trait Unbox<T> {
+	fn unbox<'a> (&'a self) -> &'a mut T;
+}
+
+impl NodeRef<CollisionObject> {
+	pub fn new(bv: CollisionObject) -> Self {
+		let node = NodeRef(Rc::new(RefCell::new(
+			Node{
+				parent: None,
+				left: None,
+				right: None,
+				bv: boxUp(bv),
+				area: Rect::new(0,0,0,0)
+			}
+		)));
+
+		node.calculateArea();
+		node
+	}
+
+	pub fn getParent(&self) -> Option<NodeRef<CollisionObject>> {
+		match &self.get().parent {
+       		Some(p) => Some(NodeRef(p.upgrade().unwrap())),
+			None => None
+		}
+	}
+
+	pub fn calculateArea(&self) {
+		if let None = self.get().left {
+			if let None = self.get().right {
+				if let None = self.get().bv {
+					// !!!!! shouldn't be possible in gameplay, here for testing !!!!!
+					self.getMut().area = Rect::new(0, 0, 0, 0); // area of 0 if node has no children and points to no collision object
+				}
+				else {
+					self.getMut().area = self.borrow().deref().rect.clone(); // area = area of collision object if node has no children but points to object
+				}
+			}
+			else {
+				self.getMut().area = self.getRightChild().get().area.clone(); // area = right if node has (None, Some)
+			}
+		}
+
+		else if let None = self.get().right {
+			self.getMut().area = self.getLeftChild().get().area.clone(); // area = left if node has (Some, None)
+		}
+		else {
+			self.getMut().area = self.getRightChild().get().area.union(self.getLeftChild().get().area); // area = smallest bounding box around both children if node has two children
+		}
+	}
+
+	pub fn overlapsWith(&self, other: NodeRef<CollisionObject>) -> bool {
+		self.get().area.has_intersection(other.get().area.clone())
+	}
+
+	pub fn borrow(&self) -> Ref<CollisionObject> {
+		Ref {_ref: self.0.borrow()}
+	}
+
+	pub fn borrow_mut(&self) -> RefMut<CollisionObject> {
+		RefMut {_ref: self.0.borrow_mut()}
+	}
+
+	pub fn getLeftChild(&self) -> NodeRef<CollisionObject> {
+		NodeRef(self.get().left.as_ref().unwrap().clone())
+	}
+
+	pub fn getRightChild(&self) -> NodeRef<CollisionObject> {
+		NodeRef(self.get().right.as_ref().unwrap().clone())
+	}
+
+	pub fn collidingWith(& self, other: NodeRef<CollisionObject>, potential: &mut Option<PotentialCollision>, limit: i32) -> i32 {
+		if !self.overlapsWith(other.clone()) || limit == 0 {return 0;}
+
+		if self.get().isLeaf() && other.get().isLeaf() {
+			potential.as_mut().unwrap().0 = self.borrow().deref().clone();
+			potential.as_mut().unwrap().1 = other.borrow().deref().clone();
+			return 1;
+		}
+
+		if other.get().isLeaf() || (!self.get().isLeaf() && self.get().area.area() >= other.get().area.area()) {
+			let count = self.getLeftChild().collidingWith(self.getRightChild(), potential, limit);
+
+			if limit > count {
+				return count + self.getRightChild().collidingWith(other, potential, limit);
+			}
+
+			else {return count;}
+		}
+
+		else {
+			let count = self.collidingWith(self.getLeftChild(), potential, limit);
+
+			if limit > count {
+				return count + self.collidingWith(self.getLeftChild(), potential, limit);
+			}
+
+			else {return count;}
+		}
+	}
+
+	pub fn getPotentialCollsions(&self, potential: &mut Option<PotentialCollision>, limit: i32) -> i32{
+		if self.get().isLeaf() || limit == 0 {return 0;}
+		self.getLeftChild().collidingWith(self.getRightChild(), potential, limit)
+	}
+
+	pub fn insert(&mut self, new_obj: CollisionObject) {
+		if self.get().isLeaf() {
+			self.getMut().left = Some(Rc::new(RefCell::new(Node::new(Some(Rc::downgrade(&self.0)), *self.borrow()._ref.bv.as_deref().unwrap()))));
+			self.getMut().right = Some(Rc::new(RefCell::new(Node::new(Some(Rc::downgrade(&self.0)), new_obj))));
+		}
+
+		else {
+			let size0 = self.getLeftChild().get().area.area();
+			let size1 = self.getRightChild().get().area.area();
+			if size0 <= size1 {
+				self.getLeftChild().insert(new_obj);
+			}
+			else {
+				self.getRightChild().insert(new_obj);
+			}
+		}
+		self.calculateArea();
+	}
+
+	pub fn remove(&mut self) {
+		if let Some(parent) = self.getParent() {
+			let mut sibling: Link<CollisionObject>;
+			if is(&parent.getLeftChild().0, &self.0) {sibling = parent.get().right.take();}
+			else {sibling = parent.get().left.take();}
+
+			parent.getMut().swap(sibling.take().unwrap());
+			parent.as_deref_mut().map(|par| {
+				par.calculateArea();
+			});
+		}
+
+		self.getLeftChild().as_deref_mut().take();
+		self.getRightChild().as_deref_mut().take();
+	}
+}
