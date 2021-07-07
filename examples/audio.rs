1d273e72284dbc45dfa3fe63773de2428728888a
Alexis Sanders <asanders@merkleinc.com>
2021-07-07 19:17:54 -0400

    adding some sh*tty audio samples for testing

--- examples/audio.rs
+++ examples/audio.rs
@@ -1,6 +1,13 @@
 extern crate street_code_fighter as scf;
 
+use std::{thread, time};
+
 fn main() {
-	scf::audio::audio::play();
-	scf::audio::audio::stop();
+	scf::audio::audio::play(); // play audio
+
+	thread::sleep(time::Duration::from_secs(10)); // wait for 10 seconds
+
+	scf::audio::audio::stop(); // stop playing audio
 }
+
+
