c299d61d4e051c50addf0d7e660083e25bb89905
Alexis Sanders <asanders@merkleinc.com>
2021-07-07 19:03:24 -0400

    setup for audio

---
+++ examples/audio.rs
@@ -0,0 +1,6 @@
+extern crate street_code_fighter as scf;
+
+fn main() {
+	scf::audio::audio::play();
+	scf::audio::audio::stop();
+}
---
+++ src/audio/audio.rs
@@ -0,0 +1,9 @@
+pub fn play() {
+	//
+	println!("play music");
+}
+
+pub fn stop() {
+	//
+	println!("stop music");
+}
